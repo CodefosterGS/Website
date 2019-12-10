fbdfe6dbb9945002ebd9502d5339af01217c6e11
Rathi <krishnam.rathi21@gmail.com>
2019-12-10 11:56:32 +0530

    update Website

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,23 +1,44 @@
-import React, { Component } from "react";
+import React from "react";
+import GitHubIcon from '@material-ui/icons/GitHub';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import InstagramIcon from '@material-ui/icons/Instagram';
 import './footer.css';
 
-class Footer extends Component {
-  render() {
-    return (
-        <div className='footer'>
-        <   div className="column">
-                <div className="roww" >
-                    <h1>Social</h1>
-                </div>
-                <div className="roww">
-                    <h1>Contact</h1>
-                </div>
-            </div>
-
+const FooterPage = () => {
+  return (
+    <div className="footer">
+      <div className="row">
+        <h3 style={{paddingTop:"20px", marginLeft:'5%'}}>Connect Us:</h3>
+        <div className="icons"> 
+          <GitHubIcon style={{fontSize: 60, href: "www.github.com"}} />
+          <FacebookIcon style={{fontSize: 60}}/>
+          <TwitterIcon style={{fontSize: 60}}/>
+          <InstagramIcon style={{fontSize: 60}}/>
         </div>
-    );
-  }
+
+        <div className="col1">
+          <h4>Recent Activities</h4>
+          <ul>
+            <li><a>Link1</a></li>
+            <li><a>Link2</a></li>
+            <li><a>Link3</a></li>
+            <li><a>Link4</a></li>
+          </ul>
+        </div>
+        <div className='col2'>
+          <h4>About us</h4>
+          <ul>
+            <li><a>Link1</a></li>
+            <li><a>Link2</a></li>
+            <li><a>Link3</a></li>
+            <li><a>Link4</a></li>
+          </ul>
+        </div>
+        <div style={{textAlign:"center", fontSize:"10px", marginTop:"30px"}}> ©2019 Codefoster</div>
+      </div>
+    </div>
+  );
 }
 
-export default Footer;
-
+export default FooterPage;
