9479cbf21a3b0f4e356ff9eda2991df6c4e91271
Ayushi0508 <ayushimjhs119@gmail.com>
2019-12-10 11:02:20 +0530

    made changes to team page

--- src/components/team.js
+++ src/components/team.js
@@ -1,12 +1,32 @@
 import React, { Component } from "react";
 import { Grid, Cell, List, ListItem, ListItemContent } from "react-mdl";
-
+import './team.css'
 class Team extends Component {
   render() {
     return (
       <div className="contact-body">
         <Grid className="contact-grid">
+          <div className="box">
           <Cell col={4}>
+            <h2>Avani Chaskar</h2>
+            <img className="logo1"
+              src="https://cdn2.iconfinder.com/data/icons/avatar-2/512/Fred_man-512.png"
+              alt="avatar"
+              style={{ height: "200px" }}
+            />
+            <p style={{ width: "75%", margin: "auto", paddingTop: "1em" }}>
+            A data science enthusiast and a leisurely competitive coder, who often motivates and mentors newbies.</p>
+            <div>
+            <a href="https://in.linkedin.com/in/avani-chaskar" class="generic-anchor" target="_blank" title="linkedin" itemprop="significantLink"><i class="fa fa-linkedin"></i></a>
+            &nbsp;
+            <a href="https://github.com/alwaysavani" class="generic-anchor" target="_blank" title="Github" itemprop="significantLink"><i class="fa fa-github"></i></a>
+            &nbsp;
+            <a href="https://in.linkedin.com/in/avani-chaskar" class="generic-anchor" target="_blank" title="Portfolio" itemprop="significantLink"><i class="fa fa-dribbble"></i></a>
+            </div>
+          </Cell>
+          </div>
+
+          {/* <Cell col={4}>
             <h2>Harshit jain</h2>
             <img
               src="https://cdn2.iconfinder.com/data/icons/avatar-2/512/Fred_man-512.png"
@@ -36,23 +56,7 @@ class Team extends Component {
               type and scrambled it to make a type specimen book. It has
               survived not only five centuries
             </p>
-          </Cell>
-
-          <Cell col={4}>
-            <h2>Harshit jain</h2>
-            <img
-              src="https://cdn2.iconfinder.com/data/icons/avatar-2/512/Fred_man-512.png"
-              alt="avatar"
-              style={{ height: "250px" }}
-            />
-            <p style={{ width: "75%", margin: "auto", paddingTop: "1em" }}>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It has
-              survived not only five centuries
-            </p>
-          </Cell>
+          </Cell> */}
 
           {/* <Cell col={6}>
             <h2>Contact Me</h2>
