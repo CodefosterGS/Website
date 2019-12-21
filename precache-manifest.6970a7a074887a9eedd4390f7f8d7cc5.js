8c9acf8f60f0039d06eea6b7a57c83b7c0085ad3
rishichawla <rishi.chawla14@gmail.com>
2019-12-21 20:47:36 +0530

    initial commit

---
+++ precache-manifest.6970a7a074887a9eedd4390f7f8d7cc5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c079957960ea001a0ee3d216d5c8faf9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7e081074e0b07333dc0",
+    "url": "/static/css/2.1d263d2c.chunk.css"
+  },
+  {
+    "revision": "3d99e867af55f96d081b",
+    "url": "/static/css/main.814ac5f7.chunk.css"
+  },
+  {
+    "revision": "b7e081074e0b07333dc0",
+    "url": "/static/js/2.9906fefc.chunk.js"
+  },
+  {
+    "revision": "e8915542158d0bc2bbcbc6853d48dbaf",
+    "url": "/static/js/2.9906fefc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3d99e867af55f96d081b",
+    "url": "/static/js/main.4269ed81.chunk.js"
+  },
+  {
+    "revision": "d10245add7450daec45b",
+    "url": "/static/js/runtime-main.ccaa913c.js"
+  },
+  {
+    "revision": "3f1971105dc34d960f177053cc3fb559",
+    "url": "/static/media/logo.3f197110.png"
+  }
+]);
