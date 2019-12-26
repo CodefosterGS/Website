df21060b4305043da7730a79e993d8cd44b9ab0c
Ayushi0508 <ayushimjhs119@gmail.com>
2019-12-26 17:37:23 +0530

    some bugs fixed

--- src/components/card.js
+++ src/components/card.js
@@ -1,44 +1,55 @@
-import React, {Component} from "react";
-import {Grid, Cell} from "react-mdl";
-import './static/card.css'
+import React, { Component } from "react";
+import { Grid, Cell } from "react-mdl";
+import "./static/card.css";
 
 class member extends Component {
-    constructor(props) { 
-        super(props);
-    }
-    render(){
+  constructor(props) {
+    super(props);
+  }
+  render() {
     return (
-       
-            <div className="contact-grid">
-            
-                     <h2>{this.props.member.name}</h2>
-                    <img className="logo1"
-                         src="https://cdn2.iconfinder.com/data/icons/avatar-2/512/Fred_man-512.png"
-                         alt="avatar"
-                    />
-                    <p className="para" > {this.props.member.description} </p>
-                    <div>
-                        <a href={this.props.member.links.linkedIn} class="generic-anchor" target="_blank"
-                           title="linkedin" itemProp="significantLink">
-                            <i class="fa fa-linkedin"/>
-                        </a>
-                        &nbsp;
-                        <a href={this.props.member.links.github} class="generic-anchor" target="_blank" title="Github"
-                           itemProp="significantLink">
-                            <i class="fa fa-github"/>
-                        </a>
-                        &nbsp;
-                        <a href={this.props.member.links.portfolio} class="generic-anchor" target="_blank"
-                           title="Portfolio" itemProp="significantLink">
-                            <i class="fa fa-dribbble"/>
-                        </a>
-                  
-                </div>
-            </div>
-     
+      <div className="contact-grid">
+        <h2>{this.props.member.name}</h2>
+        <img
+          className="circular--square"
+          src="https://cdn2.iconfinder.com/data/icons/avatar-2/512/Fred_man-512.png"
+          alt="avatar"
+        />
+        <p className="para"> {this.props.member.description} </p>
+        <div>
+          <a
+            href={this.props.member.links.linkedIn}
+            class="generic-anchor"
+            target="_blank"
+            title="linkedin"
+            itemProp="significantLink"
+          >
+            <i class="fa fa-linkedin" />
+          </a>
+          &nbsp;
+          <a
+            href={this.props.member.links.github}
+            class="generic-anchor"
+            target="_blank"
+            title="Github"
+            itemProp="significantLink"
+          >
+            <i class="fa fa-github" />
+          </a>
+          &nbsp;
+          <a
+            href={this.props.member.links.portfolio}
+            class="generic-anchor"
+            target="_blank"
+            title="Portfolio"
+            itemProp="significantLink"
+          >
+            <i class="fa fa-dribbble" />
+          </a>
+        </div>
+      </div>
     );
-    }
+  }
 }
-
 
 export default member;
--- src/components/footer.js
+++ src/components/footer.js
@@ -4,56 +4,53 @@ import TwitterIcon from "@material-ui/icons/Twitter";
 import InstagramIcon from "@material-ui/icons/Instagram";
 import "./static/footer.css";
 import React, { Component } from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 import { Container, Row } from "reactstrap";
 
-
-
 class Footer extends Component {
-    render() {
-        return (
-
-            <footer className="footer-black">
-                <Container>
-                    <Row className="foot">
-                            <div className="social">
-                                <h4>Connect With Us:</h4>
-                                <Link target="_blank" to={"//www.github.com/CodeFosterGS"} >
-                                    <GitHubIcon style={{fill:'white'}}/>
-                                </Link>
-                                &nbsp;
-                                &nbsp;
-                                <Link target="_blank" to={"//www.facebook.com/CodeFoster/"} >
-                                    <FacebookIcon style={{fill:'0084ff'}}/>
-                                </Link>
-                                &nbsp;
-                                &nbsp;
-                                <Link target="_blank" to={"//www.twitter.com/thecodefoster/"} >
-                                    <TwitterIcon style={{fill:'55acee'}}/>
-                                </Link>
-                                &nbsp;
-                                &nbsp;
-                                <Link target="_blank" to={"//www.instagram.com/codefoster.club/"} >
-                                    <InstagramIcon style={{fill:'e4405f'}}/>
-                                </Link>
-                            </div>
-                            <div className="social1">
-                                <h4 className="he">Contact:</h4>
-                                <a href="mailto:thecodefoster@gmail.com" style={{color:'white'}}>thecodefoster@gmail.com</a>
-                            </div>
-                        <div className="credits">
-                            <span className="copyright">
-                                © {new Date().getFullYear()}
-                                , made by Codefoster Web Team
-                </span>
-                        </div>
-                    </Row>
-                </Container>
-            </footer>
-
-        );
-    }
+  render() {
+    return (
+      <footer className="footer-black">
+        <Container>
+          <Row className="foot">
+            <div className="social">
+              <h4>Connect With Us:</h4>
+              <Link target="_blank" to={"//www.github.com/CodeFosterGS"}>
+                <GitHubIcon style={{ fill: "white" }} />
+              </Link>
+              &nbsp; &nbsp;
+              <Link target="_blank" to={"//www.facebook.com/CodeFoster/"}>
+                <FacebookIcon style={{ fill: "0084ff" }} />
+              </Link>
+              &nbsp; &nbsp;
+              <Link target="_blank" to={"//www.twitter.com/thecodefoster/"}>
+                <TwitterIcon style={{ fill: "55acee" }} />
+              </Link>
+              &nbsp; &nbsp;
+              <Link target="_blank" to={"//www.instagram.com/codefoster.club/"}>
+                <InstagramIcon style={{ fill: "e4405f" }} />
+              </Link>
+            </div>
+            <div className="social1">
+              <h4 className="he">Contact:</h4>
+              <a
+                href="mailto:thecodefoster@gmail.com"
+                style={{ color: "white" }}
+              >
+                thecodefoster@gmail.com
+              </a>
+            </div>
+            <div className="credits">
+              <span className="copyright">
+                © {new Date().getFullYear()}, made by Codefoster Web Team
+              </span>
+            </div>
+          </Row>
+        </Container>
+      </footer>
+    );
+  }
 }
 
 export default Footer;
